c475ffc1d3065dd97cc59d4c171c5f282c4690da
Pol-Erwan <polerwankite@gmail.com>
2019-10-17 20:23:43 +0200

    version 20h20

---
+++ app.rb
@@ -0,0 +1,14 @@
+#require 'bundle'
+require 'colorize'
+require 'pry'
+#Bundler.require
+
+require_relative 'lib/player'
+require_relative 'lib/boardCase'
+require_relative 'lib/board'
+#require_relative 'lib/game'
+require_relative 'lib/show'
+#require_relative 'lib/application'
+
+#binding.pry
+
--- lib/boardcase.rb
+++ lib/boardcase.rb
@@ -1,16 +1,25 @@
 
 class BoardCase
 
-  def initialize
+  def initialize(name)
+	@name = name
     #TO DO : doit régler sa valeur, ainsi que son numéro de case
   end
+
+puts " prénom joueur 1 ?"				# entrer son prénom
+print ">"
+player1 = Player.new(gets.chomp)
+
+puts " prénom joueur 2 ?"
+print ">"
+player2 = Player.new(gets.chomp)
 
 morpion = []
 morpiontab = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
 
-aaa = 1
+partie_on = 1
 
-while aaa == 1
+while partie_on == 1
 
 i = 0
 while i < 10
@@ -20,43 +29,47 @@ end
 
 tour_de_jeu = 1
 
-while tour_de_jeu < 10
+win = ["A","B","C","D","E","F","G","H","I"]
+
+while tour_de_jeu < 10 
+
+puts "*** % $ § % € MORPION € % § $ % *** " 
 
   puts "    1   2   3".green
   puts "  +---+---+---+".green
   print "A | ".green
-  print "#{morpion[1]}".red
+  print "#{morpion[1]}"
   print " | ".green
-  print "#{morpion[2]}".red
+  print "#{morpion[2]}"
   print " | ".green
-  print "#{morpion[3]}".red
+  print "#{morpion[3]}"
   puts " | ".green
 
   puts "  +---+---+---+".green
   print "B | ".green
-  print "#{morpion[4]}".red
+  print "#{morpion[4]}"
   print " | ".green
-  print "#{morpion[5]}".red
+  print "#{morpion[5]}"
   print " | ".green
-  print "#{morpion[6]}".red
+  print "#{morpion[6]}"
   puts " | ".green
 
   puts "  +---+---+---+".green
   print "C | ".green
-  print "#{morpion[7]}".red
+  print "#{morpion[7]}"
   print " | ".green
-  print "#{morpion[8]}".red
+  print "#{morpion[8]}"
   print " | ".green
-  print "#{morpion[9]}".red
+  print "#{morpion[9]}"
   puts " | ".green
   puts "  +---+---+---+".green
   puts " " 
 
-	if tour_de_jeu < 10
+	if tour_de_jeu <= 9 
 		if tour_de_jeu%2 == 0
-  			puts " tour n°#{tour_de_jeu}, joueur 1 : quel case ?".yellow
+  			puts " tour n°#{tour_de_jeu}, joueur 2 : quel case ?".red
 		else
-			puts " tour n°#{tour_de_jeu}, joueur 2 : quel case ?".blue
+			puts " tour n°#{tour_de_jeu}, joueur 1 : quel case ?".yellow
 		end
 
     	    position = gets.chomp
@@ -66,71 +79,81 @@ while tour_de_jeu < 10
   		if position != item
 			z = z + 1
   		else
+			system('clear')
 			puts "case #{item}."
 	
 		  if morpion[z] == " "
-
 			if tour_de_jeu%2 == 0 	
-				morpion [z] = "X" 
+				morpion [z] = "X".red 
+				win[z] = "x"
 			else
-				morpion [z] = "O"
+				morpion [z] = "O".yellow
+				win[z] = "O"
 			end
 		    tour_de_jeu = tour_de_jeu + 1
 		  else 
+		    system('clear')
 		    puts "occupé"
 		  end
   		end
 	  end
 	end
+
+	if win[1] == win[2] && win[3] == win[1]
+	puts "------ WIN LIGNE A -------" 
+	tour_de_jeu = 11
+	end
+
+	if win[4] == win[5] && win[6] == win[4]
+	puts "------ WIN LIGNE B ------ "
+	tour_de_jeu = 11
+	end
+
+	if win[7] == win[8] && win[9] == win[7]
+	puts "------ WIN LIGNE C ------ "
+	tour_de_jeu = 11
+	end
+
+	if win[1] == win[4] && win[7] == win[1]
+	puts "|||||| WIN COLONNE 1 ||||||"
+	tour_de_jeu = 11
+	end
+
+	if win[2] == win[5] && win[8] == win[2]
+	puts "|||||| WIN COLONNE 2 ||||||"
+	tour_de_jeu = 11
+	end
+
+	if win[3] == win[6] && win[9] == win[3]
+	puts "|||||| WIN COLONNE 3 ||||||"
+	tour_de_jeu = 11
+	end
+
+	if win[1] == win[5] && win[9] == win[1]
+	puts "\\\\\\ WIN DIAGONAL \\\\\\"
+	tour_de_jeu = 11
+	end
+
+	if win[3] == win[5] && win[7] == win[3]
+	puts "////// WIN DIAGONAL //////"
+	tour_de_jeu = 11
+	end
 end
 
-	puts "GAME OVER"
-  	puts "Voulez-vous recommencez une nouvelle partie ? (y/n)"
+	puts "GAME".red+"-OVER".yellow
+  	puts "Voulez-vous recommencez une nouvelle partie ? (y/n)".green
   	repeat = gets.chomp
   	print ">"
 		if repeat == "n"
-		  aaa = 0
-		  puts "non"
-		  #break
+		  partie_on = 0
+		  puts "NON".red
 		else 
-		  aaa = 1	
-		  puts "oui"					
+		  partie_on = 1	
+		  puts "OOOUUUAAAIIISSS".white					
 		end
 end
 
 
-  if tour_de_jeu > 5 
-
-	if morpion[0] == morpion[1] && morpion[2]
-	puts "WIN 0 1 2"
-	#break
-	end
-	if morpion[3] == morpion[4] && morpion[5]
-	puts "WIN 3 4 5"
-	#break
-	end
-	if morpion[6] == morpion[7] && morpion[8]
-	puts "WIN 6 7 8"
-	#break
-	end
-	if morpion[0] == (morpion[3] && morpion[6])
-	puts "WIN 0 3 6"
-	#break
-	end
-	if morpion[1] == (morpion[4] && morpion[7])
-	puts "WIN 1 4 7"
-	#break
-	end
-	if morpion[2] == (morpion[5] && morpion[8])
-	puts "WIN 2 5 8"
-	#break
-	end
-	if morpion[0] == (morpion[4] && morpion[8])
-	puts "WIN 0 4 8"
-	#break
-	end
-
-  end
 
 end
  
